05b38c621164a9b7718a4c81805b8d0553ffe50d
AmSmo <adam.smolenski@gmail.com>
2020-08-28 14:51:22 -0400

    starting

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,12 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  def city_openings(start_date, end_date)
+    self.listings.select do |listing| 
+      listing.reservations.select do |reservation|
+        Date.parse(start_date) > reservation.checkout || Date.parse(end_date) < reservation.checkin
+      end
+    end
+  end
 end
 
